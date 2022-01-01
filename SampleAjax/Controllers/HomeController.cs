7b7803411eaf39fc27520169ee8f518e690379ba
JobTracking <tuncalin@outlook.com>
2022-01-01 14:42:57 +0300

    Ajax GetAll List

--- SampleAjax/Controllers/HomeController.cs
+++ SampleAjax/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using SampleAjax.Models;
 using System;
 using System.Collections.Generic;
@@ -11,10 +12,18 @@ namespace SampleAjax.Controllers
 {
     public class HomeController : Controller
     {
+        private List<Kullanici> kullanicilar;
         private readonly ILogger<HomeController> _logger;
 
+       
         public HomeController(ILogger<HomeController> logger)
         {
+            kullanicilar = new List<Kullanici> {
+            new Kullanici{ Ad="test", Id=1},
+            new Kullanici{ Ad="test2", Id=2},
+            new Kullanici { Ad = "test3", Id = 3 },
+            new Kullanici { Ad = "test4", Id = 4 },
+            };
             _logger = logger;
         }
 
@@ -22,7 +31,16 @@ namespace SampleAjax.Controllers
         {
             return View();
         }
-
+        public IActionResult List()
+        {
+            var jsonkullanici = JsonConvert.SerializeObject(kullanicilar);
+            return Json(jsonkullanici);
+        }
+        public IActionResult GetById(int id)
+        {
+            var jsonkullanici = JsonConvert.SerializeObject(kullanicilar.Where(x => x.Id == id));
+            return Json(jsonkullanici);
+        }
         public IActionResult Privacy()
         {
             return View();
